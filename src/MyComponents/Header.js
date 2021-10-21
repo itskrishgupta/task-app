aead59396c5d3e9d5f2077c0ef7b60b28309d53d
itskrishgupta <itskrishgupta@gmail.com>
2021-10-21 12:11:07 +0530

    solving the url errors

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -8,14 +8,14 @@ const Header = (props) => {
             <>
                 <nav className="navbar navbar-expand-md navbar-dark bg-dark">
                     <div className="container-fluid">
-                        <a href="/" className="navbar-brand">{props.title}</a>
+                        <a href="/task-app/#/" className="navbar-brand">{props.title}</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="/#/">Home</a>
+                                    <a className="nav-link active" aria-current="page" href="/task-app/#/">Home</a>
                                 </li>
                                 <li className="nav-item">
                                     <a className="nav-link active" href="/task-app/#/setting">Setting</a>
